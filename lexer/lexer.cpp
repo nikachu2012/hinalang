97d0dcfc0f9238ac80826756346ef28a2c970e37
nikachu2012 <webmaster@nikachu.net>
2025-09-07 21:31:32 +0900

    [add] 数字の解析をできるように修正

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -1,5 +1,10 @@
 #include "lexer.hpp"
 
+inline bool lexer::isDecimal(char c)
+{
+    return '0' <= c && c <= '9';
+}
+
 lexer::lexer(std::ifstream &f) : fs(f)
 {
 }
@@ -27,6 +32,26 @@ next:
     case '\n':
         // ホワイトスペースの無視
         goto next;
+        break;
+
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+        // 数値の時
+        buf += c;
+        while (isDecimal(c = read()))
+        {
+            buf += c;
+        }
+        pb(); // whileの時に読みすぎるので1文字戻す
+        return LEXER_TYPE::NUMBER;
         break;
 
     default:
